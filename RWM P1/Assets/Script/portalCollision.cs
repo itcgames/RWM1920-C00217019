7b84924c67cbd9918b68d29c3d5b6909cddff0b1
SeanNash <c00217019@itcarlow.ie>
2019-11-11 23:09:20 +0000

    RWM Initial Feature Done. Basic Functionality Working

---
+++ RWM P1/Assets/Script/portalCollision.cs
@@ -0,0 +1,12 @@
+﻿	using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portalCollision : MonoBehaviour {
+
+	public Transform SpawnPoint;
+
+	void OnTriggerEnter(Collider otherObject){
+		otherObject.gameObject.transform.position = SpawnPoint.position;
+	}
+}
